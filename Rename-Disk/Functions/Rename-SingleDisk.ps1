9e070822996787e34a3b13c322372f86e6d31bda
JimMoyle <github@jimmoyle.com>
2018-04-03 14:38:04 +0100

    added rename single file

--- Rename-Disk/Functions/Rename-SingleDisk.ps1
+++ Rename-Disk/Functions/Rename-SingleDisk.ps1
@@ -1,39 +1,21 @@
-function Rename-Disk {
+function Rename-SingleDisk {
     [CmdletBinding()]
 
     Param (
         [Parameter(
-            ParameterSetName = 'File',
             Position = 0,
             ValuefromPipelineByPropertyName = $true,
             ValuefromPipeline = $true,
             Mandatory = $true
         )]
-        [System.String]$PathToDisk,
-
-        [Parameter(
-            ParameterSetName = 'Folder',
-            Position = 0,
-            ValuefromPipelineByPropertyName = $true,
-            ValuefromPipeline = $true,
-            Mandatory = $true
-        )]
-        [System.String]$Folder,
-
-        [Parameter(
-            Position = 0,
-            ValuefromPipelineByPropertyName = $true,
-            ValuefromPipeline = $true,
-            Mandatory = $true
-        )]
-        [regex]$OriginalMatch,
+        [System.String]$Path,
 
         [Parameter(
             Position = 0,
             ValuefromPipelineByPropertyName = $true,
             Mandatory = $true
         )]
-        [string]$Target,
+        [System.String]$NewName,
 
         [Parameter(
             Position = 0,
@@ -41,14 +23,20 @@ function Rename-Disk {
             Mandatory = $true
         )]
         [string]$LogDir
-
     )
 
     BEGIN {
         Set-StrictMode -Version Latest
     } # Begin
     PROCESS {
+        try{
+            Rename-Item -Path $Path -NewName $NewName -ErrorAction Stop
+            Write-Log "Renamed $Path to $NewName" -Path $LogDir
+        }
+        catch{
+            Write-Log -Level Error "Failed to rename $Path" -Path $LogDir
+        }
         
     } #Process
     END {} #End
-}  #function Rename-Disk
+}  #function Rename-SingleDisk
