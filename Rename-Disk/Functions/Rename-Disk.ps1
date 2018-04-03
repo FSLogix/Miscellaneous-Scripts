6c63aeb13695072e1f1977e210a488a29d2c9570
JimMoyle <github@jimmoyle.com>
2018-04-03 19:45:34 +0100

    Code finished before testing

--- Rename-Disk/Functions/Rename-Disk.ps1
+++ Rename-Disk/Functions/Rename-Disk.ps1
@@ -26,14 +26,14 @@ function Rename-Disk {
             ValuefromPipeline = $true,
             Mandatory = $true
         )]
-        [regex]$OriginalMatch,
+        [regex]$OriginalMatch = "^(.*?)_S-\d-\d+-(\d+-){1,14}\d+$",
 
         [Parameter(
             Position = 0,
             ValuefromPipelineByPropertyName = $true,
             Mandatory = $true
         )]
-        [string]$Target,
+        [string]$MatchesArrayNumber = 1,
 
         [Parameter(
             Position = 0,
@@ -48,7 +48,33 @@ function Rename-Disk {
         Set-StrictMode -Version Latest
     } # Begin
     PROCESS {
-        
+        switch ($PSCmdlet.ParameterSetName) {
+            Folder {
+                $files = Get-ChildItem -Path $Folder -Recurse -File -Filter *.vhd*
+            }
+            Files {
+                $files = foreach ($disk in $PathToDisk){
+                    if (Test-Path $disk){
+                        Get-ChildItem -Path $disk
+                    }
+                    else{
+                        Write-Error "$disk does not exist"
+                        Write-Log -Level Error "$disk does not exist"
+                    }
+                }
+            }
+        } #switch
+
+        foreach ($file in $files){
+            if ($file.BaseName -match $OriginalMatch){
+                $newName = $Matches["$MatchesArrayNumber"]
+                Rename-SingleDisk -Path $file -NewName $newName -LogDir $LogDir
+            }
+            else{
+                Write-Log -Level Warn "$file does not match regex"
+            }
+        }
+
     } #Process
     END {} #End
 }  #function Rename-Disk
