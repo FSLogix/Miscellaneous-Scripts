e76b5f0aee92a9a413395b796eb0704014a2178f
JimMoyle <github@jimmoyle.com>
2018-07-25 14:43:00 +0100

    initial function

---
+++ Move-FslOutlookFolder/MoveFslOutlookFolder.ps1
@@ -0,0 +1,49 @@
+function Move-FslOulookFolder {
+    [CmdletBinding(PositionalBinding=$true)]
+
+    Param (
+        [Parameter(
+            ValuefromPipelineByPropertyName = $true,
+            ValuefromPipeline = $true,
+            Mandatory = $true
+        )]
+        [System.String[]]$User,
+
+        [Parameter(
+            ValuefromPipelineByPropertyName = $true,
+            ValuefromPipeline = $true,
+            Mandatory = $true
+        )]
+        [System.String]$ProfilePath,
+
+        [Parameter(
+            ValuefromPipelineByPropertyName = $true,
+            ValuefromPipeline = $true,
+            Mandatory = $true
+        )]
+        [System.String[]]$O365Path
+    )
+
+    BEGIN {
+        Set-StrictMode -Version Latest
+        #Requires -Modules "Hyper-V"
+        #Requires -Modules "ActiveDirectory"
+        #Requires -RunAsAdministrator
+    } # Begin
+    PROCESS {
+
+        #get-sid for usernameS
+
+        #Mount profile VHD
+
+        #Mount 0365 VHD
+
+        #Copy Oulook folder
+
+        #DisMount profile VHD
+
+        #DisMount 0365 VHD
+
+    } #Process
+    END {} #End
+}  #function Move-FslOulookFolder
