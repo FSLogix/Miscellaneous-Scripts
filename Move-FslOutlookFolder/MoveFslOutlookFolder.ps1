4a084262f74e0bd774e42f1cc976a1b8a2e7f8bf
JimMoyle <github@jimmoyle.com>
2018-07-31 20:06:43 +0100

    params, log and test vhd paths

--- Move-FslOutlookFolder/MoveFslOutlookFolder.ps1
+++ Move-FslOutlookFolder/MoveFslOutlookFolder.ps1
@@ -1,5 +1,5 @@
 function Move-FslOulookFolder {
-    [CmdletBinding(PositionalBinding=$true)]
+    [CmdletBinding(PositionalBinding = $true)]
 
     Param (
         [Parameter(
@@ -11,17 +11,20 @@ function Move-FslOulookFolder {
 
         [Parameter(
             ValuefromPipelineByPropertyName = $true,
-            ValuefromPipeline = $true,
             Mandatory = $true
         )]
         [System.String]$ProfilePath,
 
         [Parameter(
             ValuefromPipelineByPropertyName = $true,
-            ValuefromPipeline = $true,
             Mandatory = $true
         )]
-        [System.String[]]$O365Path
+        [System.String]$O365Path,
+
+        [Parameter(
+            ValuefromPipelineByPropertyName = $true
+        )]
+        [System.String]$LogPath = "$Env:TEMP\Move-FslOulookFolder.log"
     )
 
     BEGIN {
@@ -32,7 +35,43 @@ function Move-FslOulookFolder {
     } # Begin
     PROCESS {
 
-        #get-sid for username
+        foreach ($account in $User) {
+
+            #Need the SID for the path
+            try {
+                $accountSID = Get-ADUser -Identity $account -ErrorAction Stop | Select-Object -ExpandProperty SID
+            }
+            catch {
+                $error[0] | Write-Log
+                Write-Log -Level Error -Message "Cannot find SID for $account, Stopping processing"
+                break
+            }
+
+            try{
+            $profileVHDPath = Join-Path -Path $ProfilePath -ChildPath (Join-Path -Path $account + '_' + $accountSID -ChildPath 'Profile_' + $account -ErrorAction Stop) -ErrorAction Stop
+
+            $o365VHDPath = Join-Path -Path $O365Path -ChildPath (Join-Path -Path $account + '_' + $accountSID -ChildPath 'ODFC_' + $account -ErrorAction Stop) -ErrorAction Stop
+            }
+            catch{
+                $error[0] | Write-Log
+                Write-Log -Level Error -Message "Cannot create vhd location paths for $account, Stopping processing"
+                break
+            }
+            if (-not (Test-Path -Path $profileVHDPath)){
+                Write-Log -Level Error -Message "Cannot find $profileVHDPath, Stopping processing $account"
+                break
+            }
+    
+            if (-not (Test-Path -Path $o365VHDPath)){
+                Write-Log -Level Error -Message "Cannot find $o365VHDPath, Stopping processing $account"
+                break
+            }
+
+
+
+
+        }
+
 
         #Mount profile VHD
 
