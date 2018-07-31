2b5b913203693bdf1e5f71d6da2b867842be6b5e
JimMoyle <github@jimmoyle.com>
2018-07-31 22:42:11 +0100

    No testing done, no pester done, but function doen

--- Move-FslOutlookFolder/MoveFslOutlookFolder.ps1
+++ Move-FslOutlookFolder/MoveFslOutlookFolder.ps1
@@ -47,16 +47,19 @@ function Move-FslOulookFolder {
                 break
             }
 
+            #Create Flip/Flop paths, won't work if customer has default path values or vhd disk files
             try{
-            $profileVHDPath = Join-Path -Path $ProfilePath -ChildPath (Join-Path -Path $account + '_' + $accountSID -ChildPath 'Profile_' + $account -ErrorAction Stop) -ErrorAction Stop
+            $profileVHDPath = Join-Path -Path $ProfilePath -ChildPath (Join-Path -Path $account + '_' + $accountSID -ChildPath 'Profile_' + $account + '.vhdx' -ErrorAction Stop) -ErrorAction Stop
 
-            $o365VHDPath = Join-Path -Path $O365Path -ChildPath (Join-Path -Path $account + '_' + $accountSID -ChildPath 'ODFC_' + $account -ErrorAction Stop) -ErrorAction Stop
+            $o365VHDPath = Join-Path -Path $O365Path -ChildPath (Join-Path -Path $account + '_' + $accountSID -ChildPath 'ODFC_' + $account + '.vhdx' -ErrorAction Stop) -ErrorAction Stop
             }
             catch{
                 $error[0] | Write-Log
                 Write-Log -Level Error -Message "Cannot create vhd location paths for $account, Stopping processing"
                 break
             }
+            
+            #make sure vhdxs really exist
             if (-not (Test-Path -Path $profileVHDPath)){
                 Write-Log -Level Error -Message "Cannot find $profileVHDPath, Stopping processing $account"
                 break
@@ -67,22 +70,64 @@ function Move-FslOulookFolder {
                 break
             }
 
+            #get last 2 free drive letters
+            $freeDrives = [char[]](68..90) | Where-Object { -not (Test-Path ($_ + ':')) } | Select-Object -Last 2
 
+            if (($freeDrives | Measure-Object | Select-Object -ExpandProperty count) -lt 2){
+                Write-Log -Level Warning 'Not enough free drive letters, trying to free spare'
+                Write-Warning 'Not enough free drive letters, trying to free spare letters'
+                get-disk | Where-Object {$_.FriendlyName -eq "Msft Virtual Disk"} | Select-Object -ExpandProperty location | Dismount-DiskImage
+                $freeDrives = [char[]](68..90) | Where-Object { -not (Test-Path ($_ + ':')) } | Select-Object -last 2
+            }
+            
 
+            #Mount both disks and assign a free drive letter
+            try{
+                Mount-VHD -Path $profileVHDPath -NoDriveLetter -Passthru -ErrorAction Stop | Get-Disk | Get-Partition | Where-Object { $_.type -eq 'Basic' } | Set-Partition -NewDriveLetter $freeDrives[0]
+            }
+            catch{
+                $error[0] | Write-Log
+                Write-Log -Level Error -Message "Cannot Mount profile disk for $account, Stopping processing"
+                break
+            }
 
+            try{
+                Mount-VHD -Path $o365VHDPath -NoDriveLetter -Passthru -ErrorAction Stop | Get-Disk | Get-Partition | Where-Object { $_.type -eq 'Basic' } | Set-Partition -NewDriveLetter $freeDrives[1]
+            }
+            catch{
+                $error[0] | Write-Log
+                Write-Log -Level Error -Message "Cannot mount o365 disk for $account, Stopping processing"
+                break
+            }
+
+            #Move Oulook folder
+            try {
+                Move-Item ( Join-Path -Path $freeDrives[0] -ChildPath 'ODFC\Outlook' ) -Destination ( Join-Path -Path $freeDrives[1] -ChildPath 'ODFC\Outlook' ) -Force
+            }
+            catch {
+                $error[0] | Write-Log
+                Write-Log -Level Error -Message "Cannot move Outlook folder for $account, Stopping processing"
+                break
+            }
+
+            try{
+                DisMount-VHD -Path $o365VHDPath -ErrorAction Stop
+            }
+            catch{
+                $error[0] | Write-Log
+                Write-Log -Level Warning -Message "Cannot DisMount o365 disk for $account"
+                Write-Warning "Cannot DisMount o365 disk for $account"
+            }
+
+            try{
+                DisMount-VHD -Path $profileVHDPath -ErrorAction Stop
+            }
+            catch{
+                $error[0] | Write-Log
+                Write-Log -Level Warning -Message "Cannot DisMount profile disk for $account"
+                Write-Warning "Cannot DisMount profile disk for $account"
+            }
         }
-
-
-        #Mount profile VHD
-
-        #Mount 0365 VHD
-
-        #Copy Oulook folder
-
-        #DisMount profile VHD
-
-        #DisMount 0365 VHD
-
     } #Process
     END {} #End
 }  #function Move-FslOulookFolder
