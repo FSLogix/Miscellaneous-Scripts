63bfc963cb5c2dbb7d9c69b514468b37bfeb7513
JimMoyle <github@jimmoyle.com>
2018-09-13 09:37:17 +0100

    Added Cleanup for failing script

--- Mount-FslDisk/Mount-FslDisk.ps1
+++ Mount-FslDisk/Mount-FslDisk.ps1
@@ -1,5 +1,4 @@
-function Mount-FslDisk
- {
+function Mount-FslDisk {
     [CmdletBinding()]
 
     Param (
@@ -17,8 +16,11 @@ function Mount-FslDisk
     } # Begin
     PROCESS {
 
+        #FSLogix Disk Partition Number
+        $partitionNumber = 1
+
         try {
-            $mountedDisk = Mount-DiskImage -ImagePath $Path  -NoDriveLetter -PassThru -ErrorAction Stop | Get-DiskImage -ErrorAction Stop
+            $mountedDisk = Mount-DiskImage -ImagePath $Path -NoDriveLetter -PassThru -ErrorAction Stop | Get-DiskImage -ErrorAction Stop
         }
         catch {
             Write-Error 'Failed to mount disk'
@@ -34,28 +36,39 @@ function Mount-FslDisk
         }
         catch {
             Write-Error "Failed to create mounting directory $mountPath"
+            #cleanup
+            $mountedDisk | Dismount-DiskImage -ErrorAction SilentlyContinue
             exit
         }
 
         try {
-            Add-PartitionAccessPath -DiskNumber $mountedDisk.Number -PartitionNumber 2 -AccessPath $mountPath -ErrorAction Stop
+
+            $addPartitionAccessPathParams = [PSCustomObject]@{
+                DiskNumber      = $mountedDisk.Number
+                PartitionNumber = $partitionNumber
+                AccessPath      = $mountPath
+                ErrorAction     = Stop
+            }
+
+            Add-PartitionAccessPath @addPartitionAccessPathParams
         }
         catch {
             Write-Error "Failed to create junction point to $mountPath"
-            Remove-Item -Path $mountPath
+            #cleanup
+            Remove-Item -Path $mountPath -ErrorAction SilentlyContinue
+            $mountedDisk | Dismount-DiskImage -ErrorAction SilentlyContinue
             exit
         }
 
         $output = [PSCustomObject]@{
-            Path = $mountPath
+            Path       = $mountPath
             DiskNumber = $mountedDisk.Number
-            ImagePath = $mountedDisk.ImagePath
+            ImagePath  = $mountedDisk.ImagePath
         }
         Write-Output $output
-        
+
     } #Process
-    END {} #End
+    END {
+
+    } #End
 }  #function Mount-FslDisk
-
-
-
